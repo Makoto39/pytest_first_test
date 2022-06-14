be785810ed5bd89bfe74c5b85db30459172842d1
makoto <valeriya-39@mail.ru>
2022-06-14 17:58:01 +0200

    19.2.3

--- tests/first_test.py
+++ tests/first_test.py
@@ -9,6 +9,11 @@ class TestCalc:
     def test_multiply_calculator_correctly(self):
         assert self.calc.multiply(self, 2, 2) == 4
 
-    def test_multiply_calculator_failed(self):
-        assert self.calc.multiply(self, 2, 2) == 5
+    def test_division_calculator_correctly(self):
+        assert self.calc.division(self, 2, 2) == 1
 
+    def test_subtraction_calculator_correctly(self):
+        assert self.calc.subtraction(self, 5, 1) == 4
+
+    def test_adding_calculator_correctly(self):
+        assert self.calc.adding(self, 5, 1) == 6
